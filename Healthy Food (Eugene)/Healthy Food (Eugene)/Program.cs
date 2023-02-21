09a5df31cc21f2cfb864075b17f100dee64b5e92
Eugene <jeka467@gmail.com>
2023-02-21 21:47:31 +0300

    Update

---
+++ Healthy Food (Eugene)/Healthy Food (Eugene)/Program.cs
@@ -0,0 +1,75 @@
+﻿var counter = 0;
+var maxCounter = 1;
+Random random = new Random();
+var min = 0;
+var max = 100;
+int theNumber = random.Next(min, max);
+var range = max - min;
+var currentDistance = 1;
+
+while(currentDistance <= range) 
+{
+    currentDistance = currentDistance * 2;
+    maxCounter++;
+}
+
+
+while (counter < maxCounter)
+{
+    Console.WriteLine($"Guess the number between {min} and {max}");
+    bool isItNumber;
+    int userGuess;
+    bool isInRange;
+
+    do
+    {
+        Console.WriteLine("What is your guess?");
+        Console.WriteLine($"You have {maxCounter - counter} tries left");
+        var userGuessString = Console.ReadLine();
+
+        isItNumber = Int32.TryParse(userGuessString, out userGuess);
+
+        if (!isItNumber)
+        {
+            Console.WriteLine("nononono. That is not a number");
+        }
+        isInRange = true;
+        if(userGuess < min|| userGuess > max)
+        {
+            isInRange = false;
+            Console.WriteLine("Number out of range");
+        }
+    } while (!isItNumber || !isInRange);
+
+
+    counter++;
+
+
+    if (userGuess == theNumber)
+    {
+      Console.WriteLine("Yes Congratulations");
+      break;
+    }
+    else if (userGuess < theNumber)
+    {
+        min = userGuess + 1;
+      Console.WriteLine("More");
+    }
+    else if (userGuess > theNumber)
+    {
+        max = userGuess - 1;
+        Console.WriteLine("Less");
+    }else
+    {
+        Console.WriteLine("Something broke");
+    }
+
+    if (counter == 11)
+    {
+        min = userGuess;
+      Console.WriteLine("You loose");
+    }
+
+    }
+
+Console.WriteLine($"It took you {counter} tries");
