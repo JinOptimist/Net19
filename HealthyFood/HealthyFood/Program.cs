55fb1d4978e8edfdf43d2a91b46bc2251e8ea81b
JinOptimist <88jin@mail.ru>
2023-02-21 19:31:57 +0300

    Add guess the number

---
+++ HealthyFood/HealthyFood/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Guess the number");
+
+var theNumber = 7;
+
+Console.WriteLine("What is you guess?");
+
+var userGuessString = Console.ReadLine();
+//var userGuess = Int32.Parse(userGuessString);
+int userGuess;
+var isItWasANumber = Int32.TryParse(userGuessString, out userGuess);
+if (!isItWasANumber)
+{
+    Console.WriteLine("You are bad persone. It was not a number");
+    return;
+}
+
+if (userGuess == theNumber)
+{
+    Console.WriteLine("Win");
+}
+else
+{
+    Console.WriteLine("Looser");
+}
+
