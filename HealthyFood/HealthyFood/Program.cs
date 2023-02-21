2c365e3b5a83a2a77bbdc528d75402d6947120c0
JinOptimist <88jin@mail.ru>
2023-02-21 21:43:00 +0300

    Done guess

--- HealthyFood/HealthyFood/Program.cs
+++ HealthyFood/HealthyFood/Program.cs
@@ -1,35 +1,65 @@
-﻿Console.WriteLine("Guess the number");
+﻿Console.WriteLine("GAME: Guess the number");
 
-var theNumber = 7;
+var min = 0;
+var max = 50;
+
+var random = new Random();
+var theNumber = random.Next(min, max);
+
 int userGuess;
 var attepmtCount = 0;
+
+var rangeOfTheNumbers = max - min;
+var attemptMaxCount = 1;
+var currentDistance = 1;
+while (currentDistance < rangeOfTheNumbers)
+{
+    currentDistance = currentDistance * 2;
+    attemptMaxCount++;
+}
 
 do
 {
     bool isItWasANumber;
     do
     {
-        Console.WriteLine("What is you guess?");
+        Console.WriteLine($"What is you guess? Range: [{min}, {max}]. Attepts: [{attepmtCount}/{attemptMaxCount}]");
         var userGuessString = Console.ReadLine();
-        
 
         isItWasANumber = Int32.TryParse(userGuessString, out userGuess);
         if (!isItWasANumber)
         {
             Console.WriteLine("It was not a number");
         }
-    } while (!isItWasANumber);
+
+        if (isItWasANumber && (userGuess > max || userGuess < min))
+        {
+            Console.WriteLine($"You guess {userGuess} not in range [{min}, {max}]");
+        }
+    } while (!isItWasANumber || userGuess > max || userGuess < min);
 
     attepmtCount++;
 
-    if (userGuess == theNumber)
+    if (userGuess > theNumber)
     {
-        Console.WriteLine("Yes");
+        Console.WriteLine($"Less then {userGuess}");
+        max = userGuess - 1;
     }
-    else
+
+    if (userGuess < theNumber)
     {
-        Console.WriteLine("No");
+        Console.WriteLine($"Greater then {userGuess}");
+        min = userGuess + 1;
     }
-} while (theNumber != userGuess);
+} while (theNumber != userGuess && attepmtCount < attemptMaxCount);
+
+if (theNumber == userGuess)
+{
+    Console.WriteLine("Win");
+}
+else
+{
+    Console.WriteLine("Loos");
+}
 
 Console.WriteLine($"Cool, you spend {attepmtCount} points");
