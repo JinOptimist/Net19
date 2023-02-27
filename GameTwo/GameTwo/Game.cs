52f8e3bc1538d2f449029f1f5983fe8cf111b7c2
AnastasiaNET19 <125586323+AnastasiaNET19@users.noreply.github.com>
2023-02-27 21:42:38 +0300

    Быки и коровы

---
+++ GameTwo/GameTwo/Game.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameTwo
+{
+    public class Game
+    {
+        public void start()
+        {
+            InPut inPut = new InPut();
+            int significance = inPut.In();
+
+            BuilderNumber builderNumber = new BuilderNumber();
+            builderNumber.BuidIt(significance);
+
+            var rule = builderNumber.Builder();
+            StartGame startGame = new StartGame(rule);
+            startGame.Start();
+            Console.Clear();
+            Console.WriteLine("Хотите сыграть еще?\n1. Да\n2. Нет");
+            string playAgain = Console.ReadLine();
+            switch (playAgain)
+            {
+                case "1":
+                    start();
+                    break;
+                case "2":
+                    Console.Clear();
+                    Console.WriteLine("Всего хорошего");
+                    break;
+                default:
+                    Console.WriteLine("Выберите один из 2 вариантов");
+                    break;
+            }
+        }
+    }
+}
