d8df27a00f1d884c40a7f4fb4a20c427430d15c8
AnastasiaNET19 <125586323+AnastasiaNET19@users.noreply.github.com>
2023-03-15 01:22:08 +0300

    ntcn

--- HealthyFood/Maze.Tests/MazeStuff/Enemies/Goblin.cs
+++ HealthyFood/Maze.Tests/MazeStuff/Enemies/Goblin.cs
@@ -9,27 +9,51 @@ namespace Maze.Tests.Enemies
 {
     public class GoblinTest
     {
+
+
+        private Mock<IMazeLevel> _maze;
+        [SetUp]
+        public void InPut()
+        {
+            _maze = new Mock<IMazeLevel>();
+        }
         [Test]
         [TestCase(10, 9, 0, 8)]
         [TestCase(1, 0, 1, 8)]
         [TestCase(-1, -2, 1, 8)]
         public void TryToStepHpGoblinHpChangeAndHeroExpChange(int hpGoblin, int hpGoblinAfterMove, int expOfHero, int hpHeroAfterOneMove)
         {
-            var mazeMock = new Mock<IMazeLevel>();
+            //var mazeMock = new Mock<IMazeLevel>();
             var heroMock = new Mock<ICharacter>();
-            mazeMock.Setup(x => x.Enemies).Returns(new List<BaseEnemy>());
+            _maze.Setup(x => x.Enemies).Returns(new List<BaseEnemy>());
             heroMock.SetupProperty(x => x.Hp);
             heroMock.Object.Hp = 9;
             heroMock.SetupProperty(x => x.Experience);
             heroMock.Object.Experience = 0;
 
-            var goblin = new Goblin(hpGoblin, 1, 1, mazeMock.Object);
-          
+            var goblin = new Goblin(hpGoblin, 1, 1, _maze.Object);
+
             goblin.TryToStep(heroMock.Object);
-            Assert.AreEqual(hpGoblinAfterMove, goblin.Hp);           
+            Assert.AreEqual(hpGoblinAfterMove, goblin.Hp);
             Assert.AreEqual(expOfHero, heroMock.Object.Experience);
             Assert.AreEqual(hpHeroAfterOneMove, heroMock.Object.Hp);
 
         }
+
+        //[Test]
+        //[TestCase(2, 2)]
+        //[TestCase(4, 7)]
+        //public void EndTurnActivityTest(int x, int y)
+        //{
+        //    _maze.Setup(x => x.Cells).Returns(new List<BaseCell>());
+
+        //    var goblin = new Goblin(1, 1, 1, _maze.Object);
+            
+           
+        //    goblin.EndTurnActivity();
+        //    Assert.AreNotEqual(x, goblin.X);
+        //    Assert.AreNotEqual(y, goblin.Y);
+        //}
     }
 }
+
