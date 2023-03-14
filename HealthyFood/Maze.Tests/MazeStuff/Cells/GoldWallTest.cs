eca6e2cb075d85dca6c1648e9fa1d1ded89f5c25
Никита <kachanovskiy88@mail.ru>
2023-03-14 21:24:22 +0300

    add test

---
+++ HealthyFood/Maze.Tests/MazeStuff/Cells/GoldWallTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Maze.MazeStuff;
+using Maze.MazeStuff.Cells;
+using Maze.MazeStuff.Characters;
+using Moq;
+using NUnit.Framework;
+
+namespace Maze.Tests.MazeStuff.Cells
+{
+	public class GoldWallTest
+	{
+        [Test]
+        public void TryToStep_CoinWall(int coinBedore, int coinafter)
+        {
+            //Step 1 Prepare
+            var mazeMock = new Mock<IMazeLevel>();
+            var heroMock = new Mock<ICharacter>();
+
+            heroMock.SetupProperty(x => x.Coins);
+
+            heroMock.Object.Coins = coinBedore;
+
+            var hardTrap = new HardTrap(1, 1, mazeMock.Object);
+
+            //Step 2 Action
+            hardTrap.TryToStep(heroMock.Object);
+
+            //Step 3 Assert
+            Assert.AreEqual(coinafter, heroMock.Object.Hp);
+        }
+    }
+}
+
