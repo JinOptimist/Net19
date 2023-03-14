77267b8a5c4a33df0053799f6219b03686bef49d
Dovakin64 <drevmog@mail.ru>
2023-03-14 21:45:07 +0300

    add2

--- HealthyFood/Maze.Tests/MazeStuff/Cells/WallTest.cs
+++ HealthyFood/Maze.Tests/MazeStuff/Cells/WallTest.cs
@@ -1,4 +1,8 @@
-﻿using NUnit.Framework;
+﻿using Maze.MazeStuff.Cells;
+using Maze.MazeStuff.Characters;
+using Maze.MazeStuff;
+using Moq;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,9 +14,43 @@ namespace Maze
     public class WallTest
     {
         [Test]
-        public void TryToStep_WallTest()
-        {
 
+        public void TryToStep_CellIsUnstepable()
+        {
+            //Step 1 Prepare
+            var mazeMock = new Mock<IMazeLevel>();
+            var heroMock = new Mock<ICharacter>();
+
+            var Wall = new Wall(1, 1, mazeMock.Object);
+
+            //Step 2 Action
+            var isSteImposible = Wall.TryToStep(heroMock.Object);
+
+            //Step 3 Assert
+            Assert.AreEqual(false, isSteImposible, "Wall must be unstepable");
+        }
+
+
+        [Test]
+        [TestCase(10, 9)]
+        [TestCase(5, 4)]
+        public void TryToStep_HeroHpChanges(int hpBeforeWall, int hpAfterWall)
+        {
+            //Step 1 Prepare
+            var mazeMock = new Mock<IMazeLevel>();
+            var heroMock = new Mock<ICharacter>();
+
+            heroMock.SetupProperty(x => x.Hp);
+
+            heroMock.Object.Hp = hpBeforeWall;
+
+            var Wall = new Wall(1, 1, mazeMock.Object);
+
+            //Step 2 Action
+            Wall.TryToStep(heroMock.Object);
+
+            //Step 3 Assert
+            Assert.AreEqual(hpAfterWall, heroMock.Object.Hp);
         }
     }
 }
