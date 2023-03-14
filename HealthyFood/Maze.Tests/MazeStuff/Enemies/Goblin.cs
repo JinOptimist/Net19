105ca721ca1a3ff7bfaf60d24609b9de0915432f
AnastasiaNET19 <125586323+AnastasiaNET19@users.noreply.github.com>
2023-03-15 00:38:58 +0300

    AllParam og trytostep

--- HealthyFood/Maze.Tests/MazeStuff/Enemies/Goblin.cs
+++ HealthyFood/Maze.Tests/MazeStuff/Enemies/Goblin.cs
@@ -10,10 +10,10 @@ namespace Maze.Tests.Enemies
     public class GoblinTest
     {
         [Test]
-        [TestCase(10, 9, 0)]
-        [TestCase(1, 0, 1)]
-        [TestCase(-1, -2, 1)]
-        public void TryToStepHpGoblinHpChangeAndHeroExpChange(int hp, int hpAfterOneMove, int expOfHero)
+        [TestCase(10, 9, 0, 8)]
+        [TestCase(1, 0, 1, 8)]
+        [TestCase(-1, -2, 1, 8)]
+        public void TryToStepHpGoblinHpChangeAndHeroExpChange(int hpGoblin, int hpGoblinAfterMove, int expOfHero, int hpHeroAfterOneMove)
         {
             var mazeMock = new Mock<IMazeLevel>();
             var heroMock = new Mock<ICharacter>();
@@ -22,10 +22,14 @@ namespace Maze.Tests.Enemies
             heroMock.Object.Hp = 9;
             heroMock.SetupProperty(x => x.Experience);
             heroMock.Object.Experience = 0;
-            var goblin = new Goblin(hp, 1, 1, mazeMock.Object);
+
+            var goblin = new Goblin(hpGoblin, 1, 1, mazeMock.Object);
+          
             goblin.TryToStep(heroMock.Object);
-            Assert.AreEqual(hpAfterOneMove, goblin.Hp);
+            Assert.AreEqual(hpGoblinAfterMove, goblin.Hp);           
             Assert.AreEqual(expOfHero, heroMock.Object.Experience);
+            Assert.AreEqual(hpHeroAfterOneMove, heroMock.Object.Hp);
+
         }
     }
 }
