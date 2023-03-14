330cb054cf5da2fc6ab815ba73f3614dd6913418
IgorKrivosheenko <igorkrivosheenko@gmail.com>
2023-03-14 21:48:12 +0300

    text redaction

--- HealthyFood/Maze.Tests/MazeStuff/Cells/GreedyHealerTest.cs
+++ HealthyFood/Maze.Tests/MazeStuff/Cells/GreedyHealerTest.cs
@@ -18,36 +18,24 @@ namespace Maze.Tests.MazeStuff.Cells
         [TestCase(5, 6, 4, 3)]
         public void TryToStep_GreedyHealerTest(int hpAfter, int hpBefore, int coinsAfter, int coinsBefore)
         {
-            //Step 1 Prepare
             var mazeMock = new Mock<IMazeLevel>();
             var heroMock = new Mock<ICharacter>();
             heroMock.SetupProperty(x => x.Hp);
             heroMock.SetupProperty(x => x.Coins);
-
             heroMock.Object.Hp = hpAfter;
             heroMock.Object.Coins = coinsAfter;
             var greedyHealer = new GreedyHealer(1, 1, mazeMock.Object);
-
-            //Step 2 Action
             var isStepPosible = greedyHealer.TryToStep(heroMock.Object);
-
-            //Step 3 Assert
             Assert.AreEqual(hpBefore, heroMock.Object.Hp);
             Assert.AreEqual(coinsBefore, heroMock.Object.Coins);
         }
         [Test]
         public void TryToPossibleStep_GreedyHealerTest()
         {
-            //Step 1 Prepare
             var mazeMock = new Mock<IMazeLevel>();
             var heroMock = new Mock<ICharacter>();
             var greedyHealer = new GreedyHealer(1, 1, mazeMock.Object);
-
-            //Step 2 Action
             var isStepPosible = greedyHealer.TryToStep(heroMock.Object);
-
-            //Step 3 Assert
-            
             Assert.AreEqual(false, isStepPosible, "Salam");
         }
     }
