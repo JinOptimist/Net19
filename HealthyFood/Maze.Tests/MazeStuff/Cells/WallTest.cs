68d68c332403fad8624b0423e5db6e4700cbd08f
Dovakin64 <drevmog@mail.ru>
2023-03-14 21:15:15 +0300

    add1

---
+++ HealthyFood/Maze.Tests/MazeStuff/Cells/WallTest.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maze
+{
+    public class WallTest
+    {
+        [Test]
+        public void TryToStep_WallTest()
+        {
+
+        }
+    }
+}
