9e768ce88899e7bf490ed8c19ca482cda64a532e
Никита <kachanovskiy88@mail.ru>
2023-03-16 11:47:54 +0300

    0.1

--- HealthyFood/Maze.Tests/MazeStuff/Cells/GoldWallTest.cs
+++ HealthyFood/Maze.Tests/MazeStuff/Cells/GoldWallTest.cs
@@ -9,25 +9,36 @@ namespace Maze.Tests.MazeStuff.Cells
 {
 	public class GoldWallTest
 	{
+        private int MaxMoney = 3;
         [Test]
-        public void TryToStep_CoinWall(int coinBedore, int coinafter)
+        //[TestCase(5,6,3,2)]
+        public void TryToStep_CoinWall(int coinBefore, int coinafter)
         {
             //Step 1 Prepare
             var mazeMock = new Mock<IMazeLevel>();
             var heroMock = new Mock<ICharacter>();
-
+            var currentCash = 3;
             heroMock.SetupProperty(x => x.Coins);
 
-            heroMock.Object.Coins = coinBedore;
-
+            heroMock.Object.Coins = coinafter;
             var hardTrap = new HardTrap(1, 1, mazeMock.Object);
+            Assert.AreEqual(coinBefore, heroMock.Object.Coins);
 
-            //Step 2 Action
-            hardTrap.TryToStep(heroMock.Object);
+            ////Step 2 Action
+            //hardTrap.TryToStep(heroMock.Object);
 
-            //Step 3 Assert
-            Assert.AreEqual(coinafter, heroMock.Object.Hp);
+            ////Step 3 Assert
+            //Assert.AreEqual(coinafter, heroMock.Object.Hp);
         }
+        //[Test]
+        //public void TryToPossiblestep_Goldwall()
+        //{
+        //    var mazeMock = new Mock<IMazeLevel>();
+        //    var heroMock = new Mock<ICharacter>();
+        //    var goldWall = new GoldWall(1, 1, mazeMock.Object);
+        //    var isStepPosible = goldWall.TryToStep(heroMock.Object);
+        //    Assert.AreEqual(false, isStepPosible, "puk");
+        //}
     }
 }
 
