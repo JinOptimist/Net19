b7e155ad4217109f1b37ebaac9f93172ac7f5366
Eugene <jeka467@gmail.com>
2023-03-23 21:27:07 +0300

    update

---
+++ HealthyFood/HealthyFoodWeb/Controllers/StoreController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HealthyFoodWeb.Controllers
+{
+    public class StoreController : Controller
+    {
+        public IActionResult storePageCatalogue()
+        {
+            return View();
+        }
+    }
+}
