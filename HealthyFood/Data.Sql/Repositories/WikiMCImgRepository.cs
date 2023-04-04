30be8e5f266d99496638d6bbd6ca90366b146a23
Valerolactone <125586170+Valerolactone@users.noreply.github.com>
2023-04-04 21:29:36 +0300

    update repository

--- HealthyFood/Data.Sql/Repositories/WikiMCImgRepository.cs
+++ HealthyFood/Data.Sql/Repositories/WikiMCImgRepository.cs
@@ -3,57 +3,32 @@ using Data.Interface.Repositories;
 
 namespace Data.Sql.Repositories
 {
-    public class WikiMCImgRepository : IWikiMcRepository
+    public class WikiMCImgRepository : BaseRepository<WikiMcImage>, IWikiMcRepository
 	{
-		private WebContext _webContext;
-
-		public WikiMCImgRepository(WebContext webContext)
-		{
-			_webContext = webContext;
-		}
-
-		public void Add(WikiMcImage model)
-		{
-			_webContext.WikiMcImages.Add((WikiMcImage)model);
-			_webContext.SaveChanges();
-		}
-
-		public WikiMcImage Get(int id)
-		{
-			return _webContext.WikiMcImages.FirstOrDefault(x => x.Id == id);
-		}
-
-		public IEnumerable<WikiMcImage> GetAll()
-		{
-			return _webContext.WikiMcImages.ToList();
-		}
+        public WikiMCImgRepository(WebContext webContext) : base(webContext) { }
 
 		public IEnumerable<WikiMcImage> GetAllImgByType(ImgTypeEnum type)
 		{
-			return _webContext.WikiMcImages.Where(x => x.ImgType == type);
+			return _dbSet.Where(x => x.ImgType == type);
 		}
 
 		public IEnumerable<WikiMcImage> GetAllImgByYear(int year)
 		{
-			return _webContext.WikiMcImages.Where(x => x.Year == year);
-		}
-
-		public void Remove(int id)
-		{
-			var mcImg = _webContext.WikiMcImages.FirstOrDefault(_x => _x.Id == id);
-			_webContext.WikiMcImages.Remove(mcImg);
+			return _dbSet.Where(x => x.Year == year);
 		}
 
 		public void RemoveAllImgByType(ImgTypeEnum type)
 		{
-			var removedType = _webContext.WikiMcImages.Where(x => x.ImgType == type).ToList();
-			removedType.ForEach(x => _webContext.WikiMcImages.Remove(x));
-		}
+			var removedType = _dbSet.Where(x => x.ImgType == type).ToList();
+			removedType.ForEach(x => _dbSet.Remove(x));
+            _webContext.SaveChanges();
+        }
 
 		public void RemoveAllImgByYear(int year)
 		{
-			var removedYear = _webContext.WikiMcImages.Where(x => x.Year == year).ToList();
-			removedYear.ForEach(x => _webContext.WikiMcImages.Remove(x));
-		}
+			var removedYear = _dbSet.Where(x => x.Year == year).ToList();
+			removedYear.ForEach(x => _dbSet.Remove(x));
+            _webContext.SaveChanges();
+        }
 	}
 }
