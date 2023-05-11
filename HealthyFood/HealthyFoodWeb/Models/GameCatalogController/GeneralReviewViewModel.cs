093eb4bc4d1669c949c5ce2b36b62d19e13dfa56
AnastasiaNET19 <125586323+AnastasiaNET19@users.noreply.github.com>
2023-05-11 20:22:03 +0300

    new

--- HealthyFood/HealthyFoodWeb/Models/GameCatalogController/GeneralReviewViewModel.cs
+++ HealthyFood/HealthyFoodWeb/Models/GameCatalogController/GeneralReviewViewModel.cs
@@ -1,8 +1,13 @@
-﻿namespace HealthyFoodWeb.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HealthyFoodWeb.Models
 {
     public class GeneralReviewViewModel
     {
         public List<ReviewViewModel> ReviewViewModels { get; set; } = new List<ReviewViewModel>();
         public string TextError { get; set; }
+
+        [Required (ErrorMessage = "Текст отзыва не может быть пустым")]
+        public string NewReview { get; set; }
     }
 }
