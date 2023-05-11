077c5824b8a41fab5f58e69ea03f6fb7af8b47b7
IgorKrivosheenko <igorkrivosheenko@gmail.com>
2023-05-11 21:19:07 +0300

    Create Catalogcontroller and view V2

---
+++ HealthyFood/HealthyFoodWeb/Controllers/CatalogControllerV2.cs
@@ -0,0 +1,26 @@
+﻿using HealthyFoodWeb.Models;
+using HealthyFoodWeb.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HealthyFoodWeb.Controllers
+{
+    public class CatalogControllerV2 : Controller
+    {
+        private IGameService _gameService;
+
+        public CatalogControllerV2(IGameService gameService)
+        {
+            _gameService = gameService;
+        }
+        public IActionResult Index(int id)
+        {
+            var allgame = _gameService.GetGameViewModel(id);
+            var recomendateGameViewModel = new GameViewModel
+            {
+                Id = allgame.Id,
+                Name = allgame.Name,
+            };
+            return View(recomendateGameViewModel);
+        }
+    }
+}
