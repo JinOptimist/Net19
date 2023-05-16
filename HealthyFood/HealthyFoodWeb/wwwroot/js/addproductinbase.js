0d3badcef703fe0822b686d7d22ac452e44f107d
PolinaRomanchuk <polinar_99@mail.ru>
2023-05-16 20:42:18 +0300

    update

---
+++ HealthyFood/HealthyFoodWeb/wwwroot/js/addproductinbase.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function () {
+
+    $('#Name').on("keyup", function () {
+        const productName = $('#Name').val();
+        $('.preview span.name').text(productName);
+    });
+
+    $('#Price').on("keyup", function () {
+        const price = $('#Price').val();
+        $('.preview span.price').text(price);
+    });
+       
+});
+
